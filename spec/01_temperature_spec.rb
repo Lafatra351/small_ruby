31b82aa03e18ba5525225b175c6323722fda5cf3
lafatra L <lafatra02@GMAIL.COM>
2020-07-30 23:46:28 +0300

    Add files via upload

---
+++ spec/01_temperature_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../lib/01_temperature'
+
+describe "temperature conversion functions" do
+
+  describe "#ftoc" do
+
+    it "converts freezing temperature" do
+      expect(ftoc(32)).to eq(0)
+    end
+
+    it "converts boiling temperature" do
+      expect(ftoc(212)).to eq(100)
+    end
+
+    it "converts body temperature" do
+      expect(ftoc(98.6)).to eq(37)
+    end
+
+    it "converts arbitrary temperature" do
+      expect(ftoc(68)).to eq(20)
+    end
+
+  end
+def "#ctof"
+puts "#ftoc".to_i.reverse()
+    end
+
+  end
+
+end
